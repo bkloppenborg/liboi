d9fe41c4f72c7f46a3e5ab796a370ba4afbbae17
Brian Kloppenborg <bkloppenborg@gmail.com>
2012-10-09 14:29:06 +0200

    Revert "Closes #18"
    This reverts commit 632ba189eace501ccd8474a4013ad76d1831547f.

--- src/COILibDataList.cpp
+++ src/COILibDataList.cpp
@@ -128,7 +128,7 @@ void COILibDataList::ReadFile(string filename)
 	// From GPAIR, Allocate storage for OIFITS data
 	oi_usersel usersel;
 	oi_data * tmp = new oi_data();
-	int status = 0;
+	int status;
 
 	// From GPAIR, read_oifits
 	strcpy(usersel.file, filename.c_str());
