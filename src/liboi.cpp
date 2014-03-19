964890fe018b5a96654affbafcefe4433f4ec4ec
Brian Kloppenborg <bkloppenborg@gmail.com>
2014-03-19 10:26:24 -0400

    clCreateFromGLTexture3D must use GL_TEXTURE_3D for all 3D OpenGL buffer types.

--- src/liboi.cpp
+++ src/liboi.cpp
@@ -824,7 +824,7 @@ void CLibOI::SetImageSource(GLuint gl_device_memory, LibOIEnums::ImageTypes type
 	case LibOIEnums::OPENGL_TEXTUREBUFFER:
 		// TODO: note that the clCreateFromGLTexture2D was depreciated in the OpenCL 1.2 specifications.
 #if defined(DETECTED_OPENCL_1_0) || defined(DETECTED_OPENCL_1_1)
-		mImage_gl = clCreateFromGLTexture3D(mOCL->GetContext(), CL_MEM_READ_ONLY, GL_TEXTURE_2D_ARRAY, 0, gl_device_memory, &status);
+		mImage_gl = clCreateFromGLTexture3D(mOCL->GetContext(), CL_MEM_READ_ONLY, GL_TEXTURE_3D, 0, gl_device_memory, &status);
 #else
 		mImage_gl = clCreateFromGLTexture(mOCL->GetContext(), CL_MEM_READ_ONLY, GL_TEXTURE_2D_ARRAY, 0, gl_device_memory, &status);
 #endif // defined(DETECTED_OPENCL_1_0) || defined(DETECTED_OPENCL_1_1)
